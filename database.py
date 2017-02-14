0aa569c8a2aabd90e6322faafe5f68e310630c4a
bmulobi <bmulobi@gmail.com>
2017-02-14 17:16:36 +0300

    Database creation script
    To be improved shortly

---
+++ database.py
@@ -0,0 +1,36 @@
+import sqlite3
+
+class create_db:
+    """Class defines method to create application database"""
+	
+    def __init__(self):
+        
+     self.table1 = "CREATE TABLE IF NOT EXISTS todo (task_id INTEGER PRIMARY KEY,task_name TEXT)"
+     self.table2 = "CREATE TABLE IF NOT EXISTS doing (row_id INTEGER PRIMARY KEY,taskID)"
+     self.table3 = "CREATE TABLE IF NOT EXISTS done (row_id INTEGER PRIMARY KEY,taskId)"
+     
+     self.connection = None
+     self.cursor = None
+     
+    def run_script(self):
+
+           try:
+               self.connection = sqlite3.connect('kanban.db')
+               self.cursor = self.connection.cursor()
+               self.cursor.execute(self.table1)
+               self.cursor.execute(self.table2)
+               self.cursor.execute(self.table3)
+
+           except sqlite3.Error as e:
+               
+               if self.connection:
+                   self.connection.rollback()
+                   return False
+           finally:
+               self.connection.close()
+           
+           return True
+
+
+		
+		
